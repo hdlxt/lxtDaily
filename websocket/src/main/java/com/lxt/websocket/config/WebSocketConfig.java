9d898e6367c02d20f99ba5359e620489d9270991
lxt.deeepin <liuxiaotong@itenscen.com>
2019-03-12 13:45:19 +0800

    save

--- websocket/src/main/java/com/lxt/websocket/config/WebSocketConfig.java
+++ websocket/src/main/java/com/lxt/websocket/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.lxt.websocket.config;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
