9a63e52e3b1b041b4bbdff255ebcfab2290b2423
lxt.deeepin <liuxiaotong@itenscen.com>
2019-03-26 13:30:52 +0800

    完善人脸识别案例；添加监测、注册、搜索演示

--- faceLogin/src/main/java/com/lxt/face/api/IndexController.java
+++ faceLogin/src/main/java/com/lxt/face/api/IndexController.java
@@ -13,25 +13,64 @@ public class IndexController {
      *登录页面
      */
     @RequestMapping
-    public String login(){
-        return "login";
+    public String face(){
+        return "face";
+    }
+    @RequestMapping("detectBrefore")
+    public String detectBrefore(){
+        return "face-detect";
+    }
+    @RequestMapping("addUserBefore")
+    public String addUserBefore(){
+        return "face-add-user";
+    }
+    @RequestMapping("searchBefore")
+    public String searchBefore(){
+        return "face-search";
     }
 
     /**
-     *
+     *  人脸检测
      * @param image
      * @return
      */
-    @RequestMapping("upload")
+    @RequestMapping("detect")
     @ResponseBody
-    public String upload(String image){
+    public String detect(String image){
         image = FaceUtil.handleFrontImage(image);
         if(image != null){
-//            return FaceUtil.detect(imageArr[1],IMAGE_TYPE_BASE64).toString();
-//            return FaceUtil.addUser(image,FaceUtil.IMAGE_TYPE_BASE64,"group1","user2").toString();
-//            return FaceUtil.search(image,FaceUtil.IMAGE_TYPE_BASE64,"group1","user1").toString();
+            return FaceUtil.detect(image,FaceUtil.IMAGE_TYPE_BASE64).toString();
         }
         return "fail！";
     }
 
+    /**
+     * 人脸注册
+     * @param image
+     * @return
+     */
+    @RequestMapping("addUser")
+    @ResponseBody
+    public String addUser(String image){
+        image = FaceUtil.handleFrontImage(image);
+        if(image != null){
+            return FaceUtil.addUser(image,FaceUtil.IMAGE_TYPE_BASE64,"group1","user2").toString();
+        }
+        return "fail！";
+    }
+
+    /**
+     * 人脸搜索
+     * @param image
+     * @return
+     */
+    @RequestMapping("search")
+    @ResponseBody
+    public String search(String image){
+        image = FaceUtil.handleFrontImage(image);
+        if(image != null){
+            return FaceUtil.search(image,FaceUtil.IMAGE_TYPE_BASE64,"group1","user1").toString();
+        }
+        return "fail！";
+    }
 }
