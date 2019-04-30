03de7f3dc935ad50ecd2fb0fbdab3a880713f404
lxt.deeepin <liuxiaotong@itenscen.com>
2019-04-30 18:40:44 +0800

    save

--- faceGps/src/main/java/com/mbyte/easy/util/BaiDuUtil.java
+++ faceGps/src/main/java/com/mbyte/easy/util/BaiDuUtil.java
@@ -1,7 +1,10 @@
 package com.mbyte.easy.util;
 
 import com.baidu.aip.face.AipFace;
+import com.baidu.aip.http.AipRequest;
+import com.baidu.aip.http.EBodyFormat;
 import com.baidu.aip.imageclassify.AipImageClassify;
+import com.mbyte.easy.admin.entity.DetectFaceVo;
 import org.json.JSONObject;
 import sun.misc.BASE64Encoder;
 
@@ -23,8 +26,9 @@ public class BaiDuUtil {
     public static final String APP_ID = "15773933";
     public static final String API_KEY = "Pi9sFpvyQxGGFEPK8CCdGpeP";
     public static final String SECRET_KEY = "SSlVhee7KcKyDR0OLMdnVFhUb7MoavBm";
-    public static final String GROUP_ID = "face_gps";
-    public static final int FACE_MIN_SCORE = 70;
+    public static final String GROUP_ID = "face_gps_new";
+    public static final String USER_ID = "face_user_id";
+    public static final int FACE_MIN_SCORE = 80;
 
     /**
      * 图片格式
@@ -34,9 +38,9 @@ public class BaiDuUtil {
      * 获取客户端
      * @return
      */
-    public static AipFace getFaceClient(){
+    public static BaiDuFace getFaceClient(){
         // 初始化一个AipFace
-        AipFace client = new AipFace(APP_ID, API_KEY, SECRET_KEY);
+        BaiDuFace client = new BaiDuFace(APP_ID, API_KEY, SECRET_KEY);
         // 可选：设置网络连接参数
         client.setConnectionTimeoutInMillis(2000);
         client.setSocketTimeoutInMillis(60000);
@@ -50,13 +54,45 @@ public class BaiDuUtil {
     public static boolean detect(String imageLocalPath) {
         // 传入可选参数调用接口
         HashMap<String, String> options = new HashMap<String, String>();
-        options.put("face_field", "age");
-        options.put("max_face_num", "1");
+        options.put("face_field", "age,face_shape,gender,eye_status,face_type");
+        options.put("max_face_num", "10");
         options.put("face_type", "LIVE");
         // 人脸检测
         JSONObject jsonObject = getFaceClient().detect(getImageStr(imageLocalPath), IMAGE_TYPE_BASE64, options);
         return jsonObject.toMap().get("result") != null;
     }
+
+    public static List<DetectFaceVo> detectNew(String imageLocalPath) {
+        // 传入可选参数调用接口
+        HashMap<String, String> options = new HashMap<String, String>();
+        options.put("face_field", "age,face_shape,gender,face_type,emotion");
+        options.put("max_face_num", "10");
+        options.put("face_type", "LIVE");
+        // 人脸检测
+        JSONObject jsonObject = getFaceClient().detect(getImageStr(imageLocalPath), IMAGE_TYPE_BASE64, options);
+        if(jsonObject.toMap().get("result") == null){
+            return null;
+        }
+        Map map = (Map)jsonObject.toMap().get("result");
+        ArrayList<Map> arrayList = ( ArrayList<Map>)map.get("face_list");
+        List<DetectFaceVo> detectFaceVos = new ArrayList<>();
+        for (Map m : arrayList) {
+            DetectFaceVo detectFaceVo = new DetectFaceVo();
+            detectFaceVo.setAge(m.get("age").toString());
+            detectFaceVo.setFaceShape(((Map)m.get("face_shape")).get("type").toString());
+            detectFaceVo.setFaceShapePro(((Map)m.get("face_shape")).get("probability").toString());
+            detectFaceVo.setFaceType(((Map)m.get("face_type")).get("type").toString());
+            detectFaceVo.setFaceTypePro(((Map)m.get("face_type")).get("probability").toString());
+            detectFaceVo.setEmotion(((Map)m.get("emotion")).get("type").toString());
+            detectFaceVo.setEmotionPro(((Map)m.get("emotion")).get("probability").toString());
+            detectFaceVo.setGender(((Map)m.get("gender")).get("type").toString());
+            detectFaceVo.setGenderPro(((Map)m.get("gender")).get("probability").toString());
+            detectFaceVos.add(detectFaceVo);
+        }
+        return detectFaceVos;
+    }
+
+
     /**
      * 人脸搜索
      * @return
@@ -66,6 +102,7 @@ public class BaiDuUtil {
         HashMap<String, String> options = new HashMap<String, String>();
         options.put("quality_control", "NORMAL");
         options.put("liveness_control", "LOW");
+        options.put("match_threshold", "60");
         options.put("max_user_num", "20");
         // 人脸搜索
         JSONObject jsonObject = getFaceClient().search(getImageStr(imageLocalPath),IMAGE_TYPE_BASE64, GROUP_ID, options);
@@ -84,6 +121,30 @@ public class BaiDuUtil {
         }
         return userIdList;
     }
+
+    /**
+     * 人脸删除接口
+     * @param faceToken
+     */
+    public static void delete(String faceToken){
+        JSONObject jsonObject =  getFaceClient().faceDelete(USER_ID,GROUP_ID,faceToken,new HashMap<String, String>());
+        System.out.println(jsonObject);
+    }
+    /**
+     *  M：1人脸搜索
+     * @return
+     */
+    public static boolean searchMul(String imageLocalPath) {
+        // 传入可选参数调用接口
+        HashMap<String, String> options = new HashMap<String, String>();
+        options.put("quality_control", "NORMAL");
+        options.put("liveness_control", "LOW");
+        options.put("max_user_num", "20");
+        options.put("max_face_num", "10");
+        // 人脸搜索
+        JSONObject jsonObject = getFaceClient().searchMul(getImageStr(imageLocalPath),IMAGE_TYPE_BASE64, GROUP_ID, options);
+        return jsonObject.toMap().get("result") != null;
+    }
     /**
      * 人脸注册
      * @param imageLocalPath
@@ -98,6 +159,21 @@ public class BaiDuUtil {
         options.put("liveness_control", "LOW");
         // 人脸注册
         return getFaceClient().addUser(getImageStr(imageLocalPath),IMAGE_TYPE_BASE64, GROUP_ID, userId, options);
+
+    }
+    public static String addUser(String imageLocalPath) {
+        // 传入可选参数调用接口
+        HashMap<String, String> options = new HashMap<String, String>();
+        options.put("user_info", "user's info");
+        options.put("quality_control", "NORMAL");
+        options.put("liveness_control", "LOW");
+        // 人脸注册
+        JSONObject jsonObject = getFaceClient().addUser(getImageStr(imageLocalPath),IMAGE_TYPE_BASE64, GROUP_ID, USER_ID, options);
+        if(jsonObject.toMap().get("result") == null){
+            return null;
+        }
+        Map map = (Map)jsonObject.toMap().get("result");
+        return map.get("face_token").toString();
 
     }
     /**
@@ -130,13 +206,16 @@ public class BaiDuUtil {
                 .append("_").append(Utility.getRandomStrByNum(6)).toString();
     }
     public static void main(String[] args) {
-        String imgLocalPath = "/home/lxt/Desktop/1.jpeg";
+        String imgLocalPath = "/home/lxt/Desktop/测试图片/合照/2";
         //人脸注册
-//        System.out.println(addUser(imgLocalPath,getUserId()));
-        //人脸搜索
-//        System.out.println(search(imgLocalPath));
+//        String faceToken = addUser(imgLocalPath);
+//        delete(faceToken);
+        //M：1人脸搜索
+//        System.out.println(searchMul(imgLocalPath));
+        //        System.out.println(search(imgLocalPath));
         //人脸监测
-//        System.out.println(detect(imgLocalPath));
+//        System.out.println(detectNew(imgLocalPath));
+
     }
 }
 
